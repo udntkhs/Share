8d3cef3c614dafa0535214e2ef6b3b58be69b61f
udntkhs <63333472+udntkhs@users.noreply.github.com>
2025-02-25 07:03:00 +0900

    Add files via upload

--- DriverInstall.bat
+++ DriverInstall.bat
@@ -1,17 +1,24 @@
-@ECHO OFF
-rem *****************************
+@echo off
+rem *****************************************************
 rem * ドライバインストール
-rem *****************************
+rem *****************************************************
 
-rem ----------------------------
+rem --------------------------------------------------
 rem * 変数
-rem ----------------------------
+rem --------------------------------------------------
 
 rem FileRepositoryのパス
-SET REPO=Y:\FileRepository
+set REPO=Y:\FileRepository
 
-echo キーを押して開始
+rem ログファイルのパス
+set LOG=Y:\FileRepository\%~n0.log
+
+
+echo 任意のキーで開始
 pause >nul
+rem --------------------------------------------------
+echo %DATE% %TIME%	【START】%~n0.bat >> %LOG%
+rem --------------------------------------------------
 
 rem FileRepository内の全ファイルを取得
 dir /s /b %REPO% > %REPO%\FileList.txt
@@ -21,13 +28,31 @@ findstr .inf$ %REPO%\FileList.txt > %REPO%\InfList.txt
 
 rem ドライバインストール
 for /f %%a in ( %REPO%\InfList.txt ) do (
-	pnputil /add-driver %%a /install
+	call :DriverInstall %%a
 )
 
 rem いらんファイルの削除
 del %REPO%\FileList.txt
 del %REPO%\InfList.txt
 
-echo おわり
+rem --------------------------------------------------
+echo %DATE% %TIME%	【FINISH】%~n0.bat >> %LOG%
+rem --------------------------------------------------
+echo 任意のキーで終了
 pause >nul
+
+
+rem --------------------------------------------------
+:DriverInstall
+rem --------------------------------------------------
+set INF=%~1
+
+rem ドライバーインストール
+pnputil /add-driver %INF% /install
+
+if %ERRORLEVEL% neq 0 (
+	echo エラー:	%INF%を追加できませんでした >> %LOG%
+)
+
+exit /b
 
